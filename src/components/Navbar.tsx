5f9a9b738f3b774f5050aa7c3bea20ef7a74672a
George Demirjian <gzdemirj@uci.edu>
2026-02-25 01:16:11 -0800

    title as full name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,14 @@ import { ThemeToggle } from "./ThemeToggle"
 
 export const Navbar = () => {
   return (
-    <div className="navbar bg-base-50 shadow-2xl">
+    <div className="navbar bg-base-50 shadow-sm shadow-slate-500 rounded-full">
   <div className="flex-none">
     <button className="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
     </button>
   </div>
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">George Demirjian's Portfolio</a>
+    <a className="btn btn-ghost text-xl">George Demirjian</a>
   </div>
   <ThemeToggle/>
   
