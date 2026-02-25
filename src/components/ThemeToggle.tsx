88434e812325bb3d9449c37e79ddab4fbc90431a
George Demirjian <gzdemirj@uci.edu>
2026-02-25 01:15:07 -0800

    change toggle color to white

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -13,11 +13,11 @@ export const ThemeToggle = () => {
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round">
-    <circle cx="12" cy="12" r="5" />
+    <circle cx="12" cy="12" r="5"/>
     <path
       d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
   </svg>
-  <input type="checkbox" value="synthwave" className="toggle theme-controller bg-amber-300 checked:border-blue-800 checked:bg-blue-300" />
+  <input type="checkbox" value="synthwave" className="toggle theme-controller text-white bg-amber-300 checked:border-blue-800 checked:bg-blue-300" />
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="20"
