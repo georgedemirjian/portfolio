219701da44dda6332bd0be7e72ca004e6cd8bd55
George Demirjian <gzdemirj@uci.edu>
2026-02-27 00:52:38 -0800

    include E&E and Projects components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
+import { EducationExperience } from './components/EducationExperience'
+import { Projects } from './components/Projects'
 
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <EducationExperience/>
+      <Projects/>
     </>
   )
 }
