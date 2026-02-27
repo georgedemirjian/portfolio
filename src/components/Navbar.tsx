6a51757f9406a6d343cf9c9557651257efe578c4
George Demirjian <gzdemirj@uci.edu>
2026-02-27 00:37:05 -0800

    make sticky navbar and include tabs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { ThemeToggle } from "./ThemeToggle"
+import {Tabs} from "./Tabs"
 
 export const Navbar = () => {
   return (
-    <div className="navbar bg-base-50 shadow-sm shadow-slate-500 rounded-full">
+    <div className="navbar bg-black sticky top-0 z-50 shadow-md">
   <div className="flex-none">
     <button className="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
     </button>
   </div>
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">George Demirjian</a>
+    <a className="btn btn-ghost text-xl">George Demirjian <Tabs/> </a>
   </div>
   <ThemeToggle/>
   
