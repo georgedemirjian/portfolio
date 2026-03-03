05867ac7eaf53b71f957b75cf4619fcdf029860a
George Demirjian <gzdemirj@uci.edu>
2026-03-02 23:12:46 -0800

    include mobile navbar, skills, and contract components

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import { Navbar } from './components/Navbar'
 import { Hero } from './components/Hero'
 import { EducationExperience } from './components/EducationExperience'
 import { Projects } from './components/Projects'
+import { Skills } from './components/Skills'
+import { Contact } from './components/Contact'
+import { MobileNavbar } from './components/MobileNavbar'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <MobileNavbar />
       <Hero/>
       <EducationExperience/>
       <Projects/>
+      <Skills/>
+      <Contact/>
     </>
   )
 }
