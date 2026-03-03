2d1c105f1dd1fcd571653650a4e15935fde358d5
George Demirjian <gzdemirj@uci.edu>
2026-03-02 22:43:49 -0800

    remove themetoggle and menu bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import { ThemeToggle } from "./ThemeToggle"
-import {Tabs} from "./Tabs"
+import { Tabs } from './Tabs'
 
 export const Navbar = () => {
   return (
-    <div className="navbar bg-black sticky top-0 z-50 shadow-md">
-  <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
-    </button>
-  </div>
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">George Demirjian <Tabs/> </a>
-  </div>
-  <ThemeToggle/>
-  
-</div>
+    <div className="bg-black sticky top-0 z-50 shadow-md hidden lg:flex">
+      <div className="navbar bg-black px-6 w-full">
+        <div className="w-1/4">
+          <a className="btn btn-ghost text-xl text-white">George Demirjian</a>
+        </div>
+        <div className="flex-1 flex justify-center">
+          <Tabs/>
+        </div>
+        <div className="w-1/4" />
+      </div>
+    </div>
   )
 }
 
